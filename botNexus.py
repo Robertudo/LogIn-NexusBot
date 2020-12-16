bceb4841122b5b5e398497d85be230dd359834fc
Robertudo <sayamosfero@gmail.com>
2020-12-16 02:43:46 -0600

    version 1

---
+++ botNexus.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains as ac
+
+try:
+	with open('data.txt', 'r') as fname:
+		data = list()
+		for line in fname:
+			splitLine = line.split(":")
+			data.append(splitLine)
+		
+		matricula = data[0][1].strip()
+		password = data[1][1].strip()
+
+		fname.close()
+
+	driver = webdriver.Chrome("C:\webdrivers\chromedriver.exe")
+
+	driver.get("https://www.uanl.mx/enlinea/")
+
+	driver.switch_to.frame('loginbox')
+
+	driver.find_element_by_xpath('//*[@id="cuenta"]').send_keys(matricula)
+
+	driver.find_element_by_xpath('//*[@id="pass"]').send_keys(password)
+
+	logIn = driver.find_element_by_xpath("/html/body/div/form/fieldset/div[4]/button")
+	logIn.click()
+
+	selectNexus = driver.find_element_by_xpath('/html/body/table[2]/tbody/tr/td[1]/table/tbody/tr[3]/td/a/img')
+	enterNexus = driver.find_element_by_xpath('//*[@id="linkNexus"]')
+
+	actions = ac(driver)
+	actions.move_to_element(selectNexus).move_to_element(enterNexus).click().perform()
+
+except Exception as e:
+	print('Agregar de manera correcta su matricula y contraseña al archivo "data.txt"')
